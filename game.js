b4935a982b58492e15c7f76b9253b81d63256d29
Volodymyr Burkovskiy <volodymyr.b.pro@gmail.com>
2021-11-17 21:59:23 +0200

    First ball take off at random angle

--- game.js
+++ game.js
@@ -11,6 +11,8 @@ let game = {
   blocks: [],
   rows: 4,
   cols: 8,
+  width: 640,
+  height: 360,
   sprites: {
     background: null,
     ball: null,
@@ -71,6 +73,7 @@ let game = {
     });
   },
   render() {
+    this.ctx.clearRect(0, 0, this.width, this.height);
     this.ctx.drawImage(this.sprites.background, 0, 0);
     this.ctx.drawImage(
       this.sprites.ball,
@@ -98,9 +101,13 @@ let game = {
       this.run();
     });
   },
+  random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
 };
 
 game.ball = {
+  dx: 0,
   dy: 0,
   velocity: 3,
   x: 320,
@@ -109,10 +116,14 @@ game.ball = {
   height: 20,
   start() {
     this.dy = -this.velocity;
+    this.dx = game.random(-this.velocity, this.velocity);
   },
   move() {
     if (this.dy) {
       this.y += this.dy;
+    }
+    if (this.dx) {
+      this.x += this.dx;
     }
   },
 };
